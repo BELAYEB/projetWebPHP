45e48b7e7ff6560277799f7ae58515e6800db22d
mahmoud belayeb <mahmoud2@gmail.com>
2025-05-22 20:28:21 +0100

    correction email register

--- pages/register.php
+++ pages/register.php
@@ -9,6 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Get and sanitize inputs
     $name = trim($_POST['name'] ?? '');
+    $email = trim($_POST['email'] ?? '');
     $password = trim($_POST['password'] ?? '');
     $confirmPassword = trim($_POST['confirmPassword'] ?? '');
     $role = "client";
@@ -25,10 +26,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     try {
-        $sql = "INSERT INTO users (name, password, role)
-                VALUES (:name, :password, :role)";
+        $sql = "INSERT INTO users (name,email,password, role)
+                VALUES (:name, :email, :password, :role)";
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':email', $email);
         $stmt->bindParam(':password', $password);
         $stmt->bindParam(':role', $role);
 
